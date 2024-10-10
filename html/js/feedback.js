0bc2ff401945476ebf2f291648a8b162d4e5b580
DangDuyLong <22010378@st.phenikaa-uni.edu.vn>
2024-10-10 20:23:37 +0700

    update

--- html/js/feedback.js
+++ html/js/feedback.js
@@ -1,4 +1,11 @@
 const btn = document.querySelector(".button");
+buttons.forEach(button) => {
+    button.addEventListener('click'), () => {
+        // Remove 'selected' class from all buttons
+        buttons.forEach(btn => btn.classList.remove('selected'));
+        // Add 'selected' class to clicked button
+        button.classList.add('selected');
+    };
 const Feedback = document.querySelector(".Feedback-container");
 btn.addEventListener("click", () => {
     Feedback.innerHTML =`<h1>Cảm ơn bạn đã phản hồi!</h1>`;
