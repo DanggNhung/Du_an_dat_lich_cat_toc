89575569725e3fa3de893581a9493eb43af86e54
DangDuyLong <22010378@st.phenikaa-uni.edu.vn>
2024-10-10 16:27:47 +0700

    update fb

--- html/js/feedback.js
+++ html/js/feedback.js
@@ -1,5 +1,5 @@
 const btn = document.querySelector(".button");
 const Feedback = document.querySelector(".Feedback-container");
 btn.addEventListener("click", () => {
-    Feedback.innerHTML =`<h1>Thank you for your feedback</h1>`;
+    Feedback.innerHTML =`<h1>Cảm ơn bạn đã phản hồi!</h1>`;
 });
